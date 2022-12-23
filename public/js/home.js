9a90cd071399ecafe21c7d9cf192d7077839d5b8
Nathiya <nadhuswt@gmail.com>
2022-12-23 20:25:26 +0530

    initial commit

---
+++ public/js/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#searchBtn").click(function () {
+        var value = $("#name").val();
+        console.log(value);
+
+        if (value == "") {
+            alert("Please enter the city name");
+            return;
+        }
+        alert("Sucess");
+    });
+});
